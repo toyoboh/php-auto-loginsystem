beea90e574be9fbfac8f5fb960440000fa2b347f
toyoboh <62339786+toyoboh@users.noreply.github.com>
2021-10-27 15:57:44 +0900

    認証クラスの作成
    認証クラスにloginメソッドを作成
    ユーザID or メールアドレス
    とパスワードを受け取ることで認証を実施する
    また、認証結果を返り値とする（連想配列）

---
+++ src/backend/Classes/Auth.php
@@ -0,0 +1,51 @@
+<?php
+namespace SToyokura\Classes;
+
+use SToyokura\Classes\DBConnect;
+
+class Auth
+{
+
+    /**
+     * 認証メソッド
+     * @param string $user_info ユーザIDもしくはメールアドレス
+     * @param string $password  パスワード
+     * @return array $login_result 認証の結果
+     */
+    public static function login($user_info, $password) {
+
+        //ユーザ情報取得のクエリ文作成
+        $sql = "SELECT user_id, user_name, password FROM t_users WHERE user_id = :user_id OR mail_address = :mail_address;";
+    
+        //インスタンス生成
+        $db = new DBConnect();
+        $pdo = $db->pdo;
+    
+        //クエリ文の実行
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindValue(":user_id", $user_info);
+        $stmt->bindValue(":mail_address", $user_info);
+        $stmt->execute();
+        $user = $stmt->fetch();
+        $count = $stmt->rowCount();
+    
+        //認証結果を返す配列を設定
+        if($count == 1 && password_verify($password, $user["password"])) {
+            $login_result = [
+                "auth" => true,
+                "user_id" => $user["user_id"],
+                "user_name" => $user["user_name"]
+            ];
+        } else {
+            $login_result = [
+                "auth" => false
+            ];
+        }
+
+        return $login_result;
+    }
+
+    public function logout() {
+
+    }
+}
