9aba8153a961cad3453d2c1e0a8583e4d6ac5ef7
toyoboh <st.0220@softbank.ne.jp>
2021-10-31 01:16:40 +0900

    Authクラス修正
    セッションにuser_idが登録されている（ログイン済み）かの確認機能追加

--- src/backend/Classes/Authentication.php
+++ src/backend/Classes/Authentication.php
@@ -15,22 +15,39 @@ class Authentication
      */
     public function login($user_info, $password) {
 
-        //条件に該当するユーザ数、ユーザ情報を取得
-        list($user_count, $user_data) = $this->selectUserData($user_info);
-    
-        //Userクラスに渡す配列を定義
-        if($user_count != 0 && password_verify($password, $user_data["password"])) {
-            $login_result_arr = $this->loginSuccessArr($user_data);
+        session_start();
+        //既にログインしていないか確認
+        if(isset($_SESSION["user_id"])) {
+            $user_data = $this->logindUserDataSelect($_SESSION["user_id"]);
+            return new User($user_data);
         } else {
-            $login_result_arr = $this->loginFailArr($user_data);
+            //条件に該当するユーザ数、ユーザ情報を取得
+            list($user_count, $user_data) = $this->notLogindUserDataSelect($user_info);
+            //パスワード確認
+            if($user_count != 0 && password_verify($password, $user_data["password"])) {
+                $login_try_result = $this->loginSuccessArr($user_data);
+            } else {
+                $login_try_result = $this->loginFailArr();
+            }
+            //Userインスタンスを生成し、返す
+            return new User($login_try_result);
         }
-        
-        //Userインスタンスを生成し、返す
-        return new User($login_result_arr);
     }
 
-    public function logout() {
-
+    /**
+     * セッションにあるuser_idを元にユーザの情報を取得する
+     * @param string $s_user_id セッションに登録されているuser_id
+     * @return array $user_data Userインスタンスに使用するユーザ情報
+     */
+    public function logindUserDataSelect($s_user_id) {
+        $sql = "SELECT user_id, user_name FROM t_users WHERE user_id = :user_id;";
+        $pdo_item_arr = [
+            "user_id" => $s_user_id
+        ];
+        $obj_use_pdo = new UsePdo($sql, $pdo_item_arr);
+        $user_data = $obj_use_pdo->stmtFetch();
+        $user_data["auth"] = true; //認証成功済みとしてauthをセット
+        return $user_data;
     }
 
     /**
@@ -39,7 +56,7 @@ class Authentication
      * @return int    $user_count 条件に該当したユーザの数
      * @return array  $user_data  ユーザの情報
      */
-    public function selectUserData($user_info) {
+    public function notLogindUserDataSelect($user_info) {
         //ユーザ情報取得のクエリ文作成
         $sql = "SELECT user_id, user_name, password FROM t_users WHERE user_id = :user_id OR mail_address = :mail_address;";
         //SQLインジェクション対策で使用する配列（bindValueで使用する）
@@ -59,22 +76,19 @@ class Authentication
     /**
      * ログイン成功時に使用するユーザ情報の配列を返すメソッド
      * @param array $user_data
-     * @return array $login_result_arr
+     * @return array $user_data
      */
     public function loginSuccessArr($user_data) {
-        return [
-            "auth" => true,
-            "user_id" => $user_data["user_id"],
-            "user_name" => $user_data["user_name"]
-        ];
+        $user_data["auth"] = true; //auth情報を追加
+        return $user_data;
     }
 
     /**
      * ログイン失敗時に使用するユーザ情報の配列を返すメソッド
-     * @param array $user_data
-     * @return array $login_result_arr
+     * @param  void
+     * @return void
      */
-    public function loginFailArr($user_data) {
+    public function loginFailArr() {
         return [
             "auth" => false
         ];
