ee5662d591efdddf007dd6a16a460a8b6bfdcef1
toyoboh <st.0220@softbank.ne.jp>
2021-11-01 00:44:39 +0900

    ログインページ表示時の処理追加
    クッキーとSession変数を使用するログイン機能の実装

--- src/backend/Classes/Token.php
+++ src/backend/Classes/Token.php
@@ -9,7 +9,7 @@ class Token
      * @param int $token_length 生成したいトークンの文字数 受け取った値に*2をした文字列になる
      * @return string 作成された文字列（トークン）
      */
-    public function getToken($token_length = 16) {
+    public static function getToken($token_length = 16) {
         $bytes = openssl_random_pseudo_bytes($token_length);
         return bin2hex($bytes);
     }
--- src/service/pages/login.php
+++ src/service/pages/login.php
@@ -1,7 +1,67 @@
 <?php
 
 
-include __DIR__ . "/../../backend/loginProcess.php";
+// include __DIR__ . "/../../backend/loginProcess.php";
+include __DIR__ . "/../../../vendor/autoload.php";
+use SToyokura\Classes\UsePdo;
+use SToyokura\Classes\Token;
+
+//sessionの有無確認
+if(!isset($_SESSION)) session_start();
+
+//sessionにuser_idが登録されていたらホーム画面へ
+if(isset($_SESSION["user_id"])) {
+    header("Location: home.php");
+    exit();
+}
+
+//cookieトークンがあったら
+if(isset($_COOKIE["cookie_token"])) {
+    $cookie_token = $_COOKIE["cookie_token"];
+    $expiration_date = new DateTime("-14 days");
+    $expiration_date = $expiration_date->format("Y-m-d H:i:s");
+    $arr = [
+        "cookie_token" => $cookie_token,
+        "expiration_date" => $expiration_date
+    ];
+    $sql = "SELECT * FROM t_auto_login WHERE cookie_token = :cookie_token AND created_at >= :expiration_date;";
+    $obj_use_pdo = new UsePdo($sql, $arr);
+    $row_count = $obj_use_pdo->stmtRowCount();
+    $row = $obj_use_pdo->stmtFetch();
+    if($row_count == 1) {
+        //token生成
+        $token = Token::getToken();
+        
+        //tokenをDBとcookieに登録
+        $insert_arr = [
+            "user_id" => $row["user_id"],
+            "cookie_token" => $token
+        ];
+        $insert_sql = "INSERT INTO t_auto_login(user_id, cookie_token) VALUES(:user_id, :cookie_token);";
+        new UsePdo($insert_sql, $insert_arr);
+
+        //cookieセット
+        setcookie("cookie_token", $token, time()+60*60*24*14, "/", null, false, true);
+
+        //古いトークンはDBから削除
+        $delete_sql = "DELETE FROM t_auto_login WHERE id = :id;";
+        $delete_arr = [ "id" => $row["id"]];
+        new UsePdo($delete_sql, $delete_arr);
+
+        //sessionにuser_idをセット
+        $_SESSION["user_id"] = $row["user_id"];
+        header("Location: home.php");
+        exit();
+    } else {
+        //古いcookieは削除
+        setcookie("cookie_token", "", time()-60);
+
+        //古いトークンはDBから削除
+        $delete_sql = "DELETE FROM t_auto_login WHERE id = :id;";
+        $delete_arr = [ "id" => $row["id"]];
+        new UsePdo($delete_sql, $delete_arr);
+    }
+}
 
 
 ?>
